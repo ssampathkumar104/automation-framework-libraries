2a0fcbc5380931df0841df317b3e8b6514f6516c
Sampath Sankati <Sampath.Sankati@infor.com>
2021-07-05 21:42:14 +0530

    Adding a project

---
+++ src/main/java/automation_framework_libraries/App.java
@@ -0,0 +1,13 @@
+package automation_framework_libraries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
